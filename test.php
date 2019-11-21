d0bd4734bed3635897416fc316706a48a1c40832
shabtai levi <iandslevi@gmail.com>
2019-11-21 12:17:20 +0200

    test 2

--- test.php
+++ test.php
@@ -74,9 +74,30 @@ if (!isset($page_url)){
 		var API_PATH = '<?= base_url('api/v1') ?>/';
 		var BASE_PATH = '<?= base_url() ?>';
 	</script>
+	<!-- Global site tag (gtag.js) - Google Analytics New
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-152960677-1"></script>
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
+	gtag('config', 'UA-152960677-1');
+	</script>-->
+
+	<!-- Google Tag Manager >
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-WR3S4V4');</script>
+	 End Google Tag Manager -->
 </head>
 <body ng-controller="MainCtrl">
+	
+	<!-- Google Tag Manager (noscript) new
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WR3S4V4"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	End Google Tag Manager (noscript) -->
 
 <header class="header">
 	<div class="header-center">
